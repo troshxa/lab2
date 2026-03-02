2a608c59aba51f52995bf0b3a76ef45bfc475a95
Wancher <54241547+troshxa@users.noreply.github.com>
2026-03-02 22:28:43 +0200

    shell sort

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ fn main() {
     let mut test = [5, 2, 9, 1, 5, 6];
     selection_sort(&mut test);
     println!("{:?}", test);
+    let mut test = [5, 2, 9, 1, 5, 6];
+    shell_sort(&mut test);
+    println!("{:?}", test);
 }
 
-pub fn selection_sort<T: Ord>(arr: &mut [T]) {
-    let Len = arr.len();
-    for i in 0..Len - 1 {
+fn selection_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    for i in 0..len - 1 {
         let mut j = i;
-        for k in i + 1..Len {
+        for k in i + 1..len {
             if arr[k] < arr[j] {
                 j = k;
             }
@@ -17,5 +20,23 @@ pub fn selection_sort<T: Ord>(arr: &mut [T]) {
         if j != i {
             arr.swap(i, j);
         }
+    }
+}
+
+fn shell_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    let mut gap = 1;
+    while gap < len / 3 {
+        gap = 3 * gap + 1;
+    }
+    while gap >= 1 {
+        for i in gap..len {
+            let mut j = i;
+            while j >= gap && arr[j - gap] > arr[j] {
+                arr.swap(j - gap, j);
+                j -= gap;
+            }
+        }
+        gap /= 3;
     }
 }
