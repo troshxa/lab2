a308446081fe22535495338006b658e972242cb5
Wancher <54241547+troshxa@users.noreply.github.com>
2026-03-02 21:34:10 +0200

    selection sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+    let mut test = [5, 2, 9, 1, 5, 6];
+    selection_sort(&mut test);
+    println!("{:?}", test);
+}
+
+pub fn selection_sort<T: Ord>(arr: &mut [T]) {
+    let Len = arr.len();
+    for i in 0..Len - 1 {
+        let mut j = i;
+        for k in i + 1..Len {
+            if arr[k] < arr[j] {
+                j = k;
+            }
+        }
+        if j != i {
+            arr.swap(i, j);
+        }
+    }
 }
