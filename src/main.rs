69564ec974dc920b1a629946ed1cbbe1daa8b48a
Wancher <54241547+troshxa@users.noreply.github.com>
2026-03-08 18:07:45 +0200

    last

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use rand::Rng;
 use std::time::Instant;
 
 fn main() {
-    let size = 100;
+    let size = 10000;
     let mut rng = rand::thread_rng();
+    
     let mut rnd_vec: Vec<i32> = (0..size).map(|_| rng.gen_range(1..=10000)).collect();
     println!("До сортування selection_sort: {:?}", rnd_vec);
     let start = Instant::now();
     selection_sort(&mut rnd_vec);
     let duration = start.elapsed();
+    
     println!("Після сортування selection_sort: {:?}", rnd_vec);
     println!("Default format: {:?}", duration.as_micros());
+
+    
     rnd_vec = (0..size).map(|_| rng.gen_range(1..=10000)).collect();
     println!("До сортування: {:?}", rnd_vec);
     let start = Instant::now();
@@ -18,6 +22,7 @@ fn main() {
     let duration = start.elapsed();
     println!("Після сортування shell_sort: {:?}", rnd_vec);
     println!("Default format: {:?}", duration.as_micros());
+    
 }
 
 fn selection_sort<T: Ord>(arr: &mut [T]) {
@@ -32,15 +37,14 @@ fn selection_sort<T: Ord>(arr: &mut [T]) {
             }
             c+=1;
         }
-        if j != i {
-            arr.swap(i, j);
-            m+=1;
-        }
+    
+        arr.swap(i, j);
+        m+=1;
     }
     println!{"Comparisons: {}, Swaps:  {}", c, m };
 }
 
-fn shell_sort<T: Ord>(arr: &mut [T]) {
+fn shell_sort<T: Ord + Copy>(arr: &mut [T]) {
     let mut c = 0;
     let mut m = 0;
     let len = arr.len();
@@ -51,18 +55,23 @@ fn shell_sort<T: Ord>(arr: &mut [T]) {
     while gap >= 1 {
         for i in gap..len {
             let mut j = i;
+            let key = arr[j];
             while j >= gap {
                 c+=1;
-                if arr[j - gap] > arr[j]{
-                    arr.swap(j - gap, j);
+                if arr[j - gap] > key {
+                    
+                    arr[j] = arr[j - gap];
                     m+=1;
                     j -= gap; 
                 }else{
                     break;
                 }
+                
                  
                 
             }
+            arr[j] = key;
+            m+=1;
         }
         gap /= 3;
     }
